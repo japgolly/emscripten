403d0e8fdfcbe08012a2b0e83a0652df052f316f
Alon Zakai <alonzakai@gmail.com>
2013-08-26 14:22:21 -0700

    forward parent Module to side module

--- src/library.js
+++ src/library.js
@@ -5042,7 +5042,8 @@ LibraryManager.library = {
       try {
         var lib_module = eval(lib_data)(
 #if ASM_JS
-          asm.maxFunctionIndex
+          asm.maxFunctionIndex,
+          Module
 #else
           {{{ Functions.getTable('x') }}}.length
 #endif
